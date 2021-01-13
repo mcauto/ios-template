bff785a486ac3d3ff4dbb60a61508ec366f2a04c
Kim Mincheol <mcauto@naver.com>
2021-01-13 20:03:16 +0900

    Initial Commit

---
+++ template/templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  templateApp.swift
+//  template
+//
+//  Created by deo on 2021/01/13.
+//
+
+import SwiftUI
+
+@main
+struct templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
